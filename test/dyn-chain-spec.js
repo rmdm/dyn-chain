195c6e1c619c3b3652ae0be0f0ac533ce172da61
rmdm <rmdm@users.noreply.github.com>
2014-04-15 21:10:26 +0400

    Tests fixed.

--- test/dyn-chain-spec.js
+++ test/dyn-chain-spec.js
@@ -2,51 +2,59 @@ describe('Dynamic chain', function(){
  
   var chain = require('../lib/dyn-chain');
   
-  chain.define('b', ['a'], function(a){
-    return function(){
-	  return a() + 4;
-	};
+  beforeEach(function(){  
+    chain.define('b', ['a'], function(a){
+      return function(){
+	    return a() + 4;
+	  };
+    });  
+    chain.define('a', [], function(){
+      return function(){
+	    return 5;
+	  };
+    });  
   });
   
-  chain.define('a', [], function(){
-    return function(){
-	  return 5;
-	};
+  afterEach(function(){
+    chain.remove('a');
+	chain.remove('b');
   });
 
   it('exec() should execute define()-d functions in chain. They can be define()-d independently from each other', function(){
     expect(chain.exec('b')).toEqual(9);
   });
-  
-  chain.redefine('b', function(a){
-    return function(){
-	  return a() * 4;
-	};
+    
+  it('We able to call different members of the chain', function(){
+    expect(chain.exec('a')).toEqual(5);
   });
   
-  it('exec() still should return expected results after we redefine() functions', function(){
+  it('exec() still should return expected results after we redefine() functions', function(){  
+    chain.redefine('b', function(a){
+      return function(){
+	    return a() * 4;
+	  };
+    });	
     expect(chain.exec('b')).toEqual(20);
   });
-  
-  chain.redefine('a', function(){
-    return function(){
-	  return 10;
-	};
-  });
-  
-  it('And even more redefine().', function(){
+    
+  it('And even more redefine().', function(){    
+	chain.redefine('b', function(a){
+      return function(){
+	    return a() * 4;
+	  };
+    });	
+	chain.redefine('a', function(){
+      return function(){
+	    return 10;
+	  };
+    });  
     expect(chain.exec('b')).toEqual(40);
   });
   
-  it('We able to call different members of the chain', function(){
-    expect(chain.exec('a')).toEqual(10);
-  });
-  
-  chain.remove('a');
-  
-  it('Should throw error when we delete some dependents from the chain', function(){
-    expect(chain.exec('b')).toThrow();
-	expect(chian.exec('a')).toThrow();
+  it('Should throw error when we delete some dependents from the chain', function(){    
+    chain.remove('a');    
+    expect(function(){chain.exec('b');}).toThrow();
+	expect(function(){chian.exec('a');}).toThrow();
   });
   
 });

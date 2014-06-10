844be8b1dec043d0f4267a38811186136303954d
rmdm <rmdm@users.noreply.github.com>
2014-06-11 00:31:13 +0400

    Simplified implementation with interface changes

--- test/dyn-chain-spec.js
+++ test/dyn-chain-spec.js
@@ -1,20 +1,20 @@
 describe('Dynamic chain', function(){
- 
+
   var chain = require('../lib/dyn-chain');
-  
-  beforeEach(function(){  
+
+  beforeEach(function(){
     chain.define('b', ['a'], function(a){
       return function(){
         return a() + 4;
       };
-    });  
+    });
     chain.define('a', [], function(){
       return function(){
         return 5;
       };
-    });  
+    });
   });
-  
+
   afterEach(function(){
     chain.remove('a');
     chain.remove('b');
@@ -23,38 +23,38 @@ describe('Dynamic chain', function(){
   it('exec() should execute define()-d functions in chain. They can be define()-d independently from each other', function(){
     expect(chain.exec('b')).toEqual(9);
   });
-    
+
   it('We able to call different members of the chain', function(){
     expect(chain.exec('a')).toEqual(5);
   });
-  
-  it('exec() still should return expected results after we redefine() functions', function(){  
-    chain.redefine('b', function(a){
+
+  it('exec() still should return expected results after we redefine functions', function(){
+    chain.define('b', ['a'], function(a){
       return function(){
         return a() * 4;
       };
-    });	
+    });
     expect(chain.exec('b')).toEqual(20);
   });
-    
-  it('And even more redefine().', function(){    
-    chain.redefine('b', function(a){
+
+  it('And even more redefine().', function(){
+    chain.define('b', ['a'], function(a){
         return function(){
           return a() * 4;
         };
-    });	
-    chain.redefine('a', function(){
+    });
+    chain.define('a', [], function(){
         return function(){
           return 10;
         };
-    });  
+    });
     expect(chain.exec('b')).toEqual(40);
   });
-  
-  it('Should throw error when we delete some dependents from the chain', function(){    
-    chain.remove('a');    
+
+  it('Should throw error when we delete some dependents from the chain', function(){
+    chain.remove('a');
     expect(function(){chain.exec('b');}).toThrow();
     expect(function(){chian.exec('a');}).toThrow();
   });
-  
+
 });
--- test/dyn-chain-with-objs-spec.js
+++ test/dyn-chain-with-objs-spec.js
@@ -1,44 +1,44 @@
 describe('Dynamic chain', function(){
-  
+
   var chain = require('../lib/dyn-chain');
-  
-  beforeEach(function(){    
+
+  beforeEach(function(){
     function A(){
       this.a = "a";
     }
-    
+
     A.prototype.fa = function(){
       return this.a;
     };
-    
+
     function B(){
       this.b = "b";
     }
-    
+
     B.prototype.fb = function(a){
       return this.b + a();
     };
-    
+
     var oa = new A();
     var ob = new B();
-    
-    chain.define('a', [], function(){return oa.fa;}, oa);	
-    
+
+    chain.define('a', [], function(){return oa.fa.bind(oa);});
+
     chain.define('b', ['a'], function(a){
       return function(){
-        return ob.fb(a);
+        return ob.fb.bind(ob)(a);
       };
-    }, ob);  
+    });
   });
-  
+
   afterEach(function(){
     chain.remove('a');
     chain.remove('b');
   });
-  
+
   it('should work with functions that have "this" in them', function(){
     expect(chain.exec('a')).toEqual('a');
     expect(chain.exec('b')).toEqual('ba');
-  });  
-  
+  });
+
 });
